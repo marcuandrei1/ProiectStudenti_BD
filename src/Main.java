130128fe32897527354f4c6d5e694bf3b8188c81
Admin <jorjebodea1@gmail.com>
2024-11-30 14:42:58 +0200

    am adaugat conexiunea cu baza de date

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+
+import java.sql.*;
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        try{
+            String url="jdbc:mysql://139.144.67.202:3306/lms?user=lms&password=WHlQjrrRDs5t";
+            Connection conn= DriverManager.getConnection(url);
 
-        System.out.println("Ce faci");
+            Statement s=conn.createStatement();
+            ResultSet rs=s.executeQuery("SELECT * FROM adresa");
+
+            while(rs.next()){
+                System.out.println(rs.getString("strada"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
